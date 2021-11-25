da734f85f900d241da6b2d1e6b8f8fbbc8b424f6
JH <magatriod7@naver.com>
2021-11-25 16:46:56 +0900

    feat: chicken

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // fibona chicken 1 1 2 3 3 4 
-people = 0
+people = 10
 peoplearr = []
 fiboarr = 0
 chicken = 0
@@ -8,6 +8,21 @@ peoplearr.push(1)
 peoplearr.push(1)
 for(a = 0; a<30; a++){
 	peoplearr.push(peoplearr[a]+peoplearr[a+1])
-	console.log("str" + a + ":" + peoplearr[a+2])
 
 }
+a = 0;
+while((people != 0)&&a<10){
+while(a<10){
+	console.log("in")
+	if(peoplearr[a]<people){
+		a++
+	}
+	else{
+		console.log("sub")
+		people-=peoplearr[a-1]
+		chicken+=(a-1)
+		break;
+	}
+}
+}
+console.log(chicken)
