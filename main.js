24762db32a04a090829363cbc85960ad4f250aa4
JH <magatriod7@naver.com>
2021-11-25 16:35:34 +0900

    feat: Fibo arr done

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // fibona chicken 1 1 2 3 3 4 
 people = 0
 peoplearr = []
-calcnt = 0
-calcnt = 0
-for(a = 0; a<101; a++){
-	
-
+fiboarr = 0
+chicken = 0
+a = 0;
+peoplearr.push(1)
+peoplearr.push(1)
+for(a = 0; a<30; a++){
+	peoplearr.push(peoplearr[a]+peoplearr[a+1])
+	console.log("str" + a + ":" + peoplearr[a+2])
 
 }
